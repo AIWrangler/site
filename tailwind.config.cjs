7eeeecedc8cd85c1897d2d55ba5488aeca1e6f62
Gary Wu <38446+garywu@users.noreply.github.com>
2023-02-19 19:26:21 -0600

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,5 +9,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/line-clamp')],
+  plugins: [require("@tailwindcss/typography"), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/line-clamp'), require("daisyui")],
 };
