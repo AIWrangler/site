7dd7593ee21752f76471e57ffcf94d6fd02eede8
Gary Wu <38446+garywu@users.noreply.github.com>
2023-03-02 12:43:17 -0600

    update

--- src/utils/getBrands.js
+++ src/utils/getBrands.js
@@ -1,6 +1,6 @@
 import { useSanityClient, groq } from "astro-sanity";
 
-export default async function index(limit=0) {
+export async function Brands(limit=0) {
   let brands = await useSanityClient().fetch(
     groq`*[_type == "brand"]{title, 'slug':slug.current, 'subtitle':description, body, 'image':{'src': mainImage.asset->url}, 'stars':rating, 'tags':tags[]->title, links[]->{url, 'logo':type->logo.asset->url}}`
   );
@@ -10,4 +10,16 @@ export default async function index(limit=0) {
   }
 
   return brands
+}
+
+export async function TextToImages(limit=0) {
+  let images = await useSanityClient().fetch(
+    groq`*[_type == "textToImage"]{title, 'slug':slug.current, prompt, negativePrompt, 'image':image.asset->url, 'tools':tool[]->{'slug':slug.current, title}}`
+  );
+
+  if(limit){
+      return images.slice(0, limit)
+  }
+
+  return images
 }
