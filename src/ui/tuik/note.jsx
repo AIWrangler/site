2b72f10be18fc231165dff39cac3f279e547ee14
Gary Wu <38446+garywu@users.noreply.github.com>
2023-03-03 12:56:34 -0600

    update

--- src/ui/tuik/note.jsx
+++ src/ui/tuik/note.jsx
@@ -16,7 +16,9 @@ const Index = (props) => {
       <div className="mx-auto container">
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {list.map((item) => (
-            <a href={`/resources/${item.slug}`}>
+            <a
+              href={`/resources/${item.slug}`}
+              class="hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
               <div className="w-full h-72 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4 hover:bg-slate-200">
                 <div>
                   <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
@@ -29,7 +31,7 @@ const Index = (props) => {
                     <div className="w-42 h-24 bg-cover bg-center">
                       <img
                         src={item.image.src}
-                        alt={item.image.alt?item.image.alt:item.title}
+                        alt={item.image.alt ? item.image.alt : item.title}
                         className="h-full w-full overflow-hidden object-cover border-2 border-white dark:border-gray-700 shadow"
                       />
                     </div>
